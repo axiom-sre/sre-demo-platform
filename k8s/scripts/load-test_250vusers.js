222b583b01e913935c12d35f6046771886e41f67
axiom-sre <nubinismail+sre@gmail.com>
2026-04-24 21:23:06 -0500

    snapshot: eod 2026-04-24 [scripts,load-testing]
    Changes
    - Areas touched : scripts,load-testing
    - Added         : 4 file(s)
    - Modified      : 0 file(s)
    - Deleted       : 0 file(s)
    
    Files
     M k8s/scripts/k6-job-1000vu.yaml
     M k8s/scripts/load-test_100vusers.js
    ?? k8s/load-testing/
    ?? k8s/scripts/load-test_1000vusers.js
    ?? k8s/scripts/load-test_250vusers.js
    ?? k8s/scripts/load-test_500vusers.js

--- k8s/scripts/load-test_250vusers.js
+++ k8s/scripts/load-test_250vusers.js
@@ -35,8 +35,8 @@ const PRODUCTS = [
 export const options = {
   stages: [
     { duration: '1m', target: 10 },
-    { duration: '1m', target: 100 },
-    { duration: '5m',  target: 100 },
+    { duration: '1m', target: 250 },    
+    { duration: '5m',  target: 250 },
     { duration: '1m', target: 0  },
   ],
   thresholds: {
--- k8s/scripts/load-test_500vusers.js
+++ k8s/scripts/load-test_500vusers.js
@@ -34,10 +34,10 @@ const PRODUCTS = [
 
 export const options = {
   stages: [
-    { duration: '1m', target: 10 },
-    { duration: '1m', target: 100 },
-    { duration: '5m',  target: 100 },
-    { duration: '1m', target: 0  },
+    { duration: '1m', target: 50 },
+    { duration: '2m', target: 500 },    
+    { duration: '5m',  target: 500 },
+    { duration: '2m', target: 0  },
   ],
   thresholds: {
     http_req_failed:   ['rate<0.001'],
